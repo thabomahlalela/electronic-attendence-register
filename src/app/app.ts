d7d8cbaf2514b0d958aa03dd59708c5f3c604bb2
Peggy <peggydudu5@gmail.com>
2025-08-26 15:50:56 +0200

    Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientAdminHome } from './components/client-admin-home/client-admin-home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ClientAdminHome],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
