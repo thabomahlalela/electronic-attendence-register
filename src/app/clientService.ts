ebe02bd106f7686373dd20e4d332db0ef1802f35
dudu <dudurose05@gmail.com>
2025-08-27 12:09:58 +0200

    commit

--- src/app/clientService.ts
+++ src/app/clientService.ts
@@ -1,14 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Company } from "./models/company.models";
 
 @Injectable({
     providedIn : 'root'
 })
 export class ClientService{
-     private apiUrl = '/api/companies';
+    //  private apiUrl = '/api/companies';
 
 
-      constructor(private http:HttpClient){}
+    //   constructor(private http:HttpClient){}
+
+      registerCompany(company : Company) :void{
+
+        
+      }
 
     
 
