e34e57f7fbb1a9080f4574b1641fb5a39747807c
Jabu <sibiyajabu21@gmail.com>
2025-08-29 12:20:32 +0200

    user-edit

---
+++ src/app/edit-user.spec.ts
@@ -0,0 +1,8 @@
+import { EditUser } from './edit-user';
+
+describe('EditUser', () => {
+  it('should create an instance', () => {
+    const directive = new EditUser();
+    expect(directive).toBeTruthy();
+  });
+});
